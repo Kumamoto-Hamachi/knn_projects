cf063a19e3e74761b0fd72c8bc4dc121a6fc045c
Kumamoto <onioni9999nagare@gmail.com>
2021-04-30 00:04:32 +0900

    update/optimize knn

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
+from util.util import argmax_for_dict
+
 """
 (1)heatmap
 why:
@@ -37,11 +39,22 @@ def experiment_knn_score(X, y):
     return scores
 
 
+def optimize(X, y, flg_calc=False):
+    if flg_calc:
+        scores = experiment_knn_score(X, y)
+    else:
+        scores = pickle.load(open("pickles/scores.pickle", "rb"))
+    opt_k, max_score = argmax_for_dict(scores)
+    return opt_k
+
+
 if __name__ == "__main__":
-    #X, y = load_mnist()
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=60000, shuffle=False)  # the mnist dataset have already shuffled
+    X, y = load_mnist()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=60000, shuffle=False)  # the mnist dataset have already shuffled
     #scores = experiment_knn_score(X_train, y_train)
     #print("scores", scores)  # debug
     #pickle.dump(scores, open("pickles/scores.pickle", "wb"))
-    scores = pickle.load(open("pickles/scores.pickle", "rb"))
-    print("scores", scores)  # debug
+    #scores = pickle.load(open("pickles/scores.pickle", "rb"))
+    #print("scores", scores)  # debug
+    opt_k = optimize(X_train, y_train)
+    print("opt_k", opt_k)  # debug
