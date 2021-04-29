8cb7bac26b4884a51445d2672a1a4d880573c50f
Kumamoto <onioni9999nagare@gmail.com>
2021-04-29 22:29:02 +0900

    update/train_test_split

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from pprint import pprint as pp
 from pprint import pformat as pf
 from sklearn.datasets import fetch_openml
 import pickle
+from sklearn.model_selection import train_test_split
 
 """
 (1)heatmap
 why:
 goal:
 """
+
 
 def load_mnist(flg_fetch=False):
     fname = "pickles/mnist.pickle"
@@ -21,5 +23,7 @@ def load_mnist(flg_fetch=False):
     print("len(X), len(y)", len(X), len(y))  # debug
     return X, y
 
+
 if __name__ == "__main__":
-    load_mnist()
+    X, y = load_mnist()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=60000, shuffle=False)  # the mnist dataset have already shuffled
