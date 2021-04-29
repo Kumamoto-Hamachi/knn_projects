6bc44e015bd44a7fe9631c2c0230b4edf42bc51e
Kumamoto <onioni9999nagare@gmail.com>
2021-04-29 23:10:39 +0900

    update/experiment_knn_score

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pprint import pformat as pf
 from sklearn.datasets import fetch_openml
 import pickle
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 """
 (1)heatmap
@@ -24,6 +25,23 @@ def load_mnist(flg_fetch=False):
     return X, y
 
 
+def experiment_knn_score(X, y):
+    scores = {}
+    X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.8, shuffle=False)
+    for neigh_num in range(1, 6):
+        knn = KNeighborsClassifier(n_neighbors=neigh_num)
+        knn.fit(X_train, y_train)
+        score = knn.score(X_val, y_val)
+        print("score", score)  # debug
+        scores[neigh_num] = score
+    return scores
+
+
 if __name__ == "__main__":
-    X, y = load_mnist()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=60000, shuffle=False)  # the mnist dataset have already shuffled
+    #X, y = load_mnist()
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=60000, shuffle=False)  # the mnist dataset have already shuffled
+    #scores = experiment_knn_score(X_train, y_train)
+    #print("scores", scores)  # debug
+    #pickle.dump(scores, open("pickles/scores.pickle", "wb"))
+    scores = pickle.load(open("pickles/scores.pickle", "rb"))
+    print("scores", scores)  # debug
