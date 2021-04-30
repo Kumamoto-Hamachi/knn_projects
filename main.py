0663f29fe2c6a20a21ae658bc55413ccf9973441
Kumamoto <onioni9999nagare@gmail.com>
2021-04-30 21:28:14 +0900

    update/load knn, mnist

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from sklearn.model_selection import train_test_split
+
+from mnist import load_mnist, learn_knn
+OPT_K=3  # this num is calced by knn.py
+
+
 if __name__ == "__main__":
-    pass
+    X, y = load_mnist()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=60000, shuffle=False)  # the mnist dataset have already shuffled
+    knn = learn_knn(X_train, y_train, OPT_K)
